bcb945412f06bc270361dac1898d9f5f35b709e2
FeelipePM <feelipe@email.com>
2022-02-02 10:18:16 -0300

    Folder structure

--- src/schemas/user/resolvers.js
+++ src/schemas/user/resolvers.js
@@ -1,15 +1,18 @@
 const users = [
   {
+    id: 1,
     name: 'John Doe 1',
     email: 'johndoe1@gmail.com',
     age: 31
   },
   {
+    id: 2,
     name: 'John Doe 2',
     email: 'johndoe2@gmail.com',
     age: 24
   },
   {
+    id: 3,
     name: 'John Doe 3',
     email: 'johndoe3@gmail.com',
     age: 19
@@ -19,5 +22,6 @@ const users = [
 export const resolvers = {
   Query: {
     users: () => users,
+    user: () => users[0],
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import { ApolloServer } from 'apollo-server';
-import { resolvers } from './src/resolvers.js';
+import { resolvers } from './schemas/user/resolvers.js';
 import { importSchema } from 'graphql-import';
 
-const typeDefs = importSchema('./src/schema.graphql');
+const typeDefs = importSchema('./src/schemas/user/schema.graphql');
 
 const server = new ApolloServer({ 
   typeDefs: typeDefs, 
   resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });

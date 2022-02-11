1d03545ae38c9e45c2b05bcb18cbcf559e7b0450
FeelipePM <feelipe@email.com>
2022-02-11 16:12:57 -0300

    fixed migrations

--- prisma/migrations/20220207134058_roles/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "roles" (
-    "name" TEXT NOT NULL,
-    "code" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "roles_pkey" PRIMARY KEY ("code")
-);
--- prisma/migrations/20220207134118_permissions/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "permissions" (
-    "description" TEXT NOT NULL,
-    "code" TEXT NOT NULL,
-    "role_id" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "permissions_pkey" PRIMARY KEY ("role_id")
-);

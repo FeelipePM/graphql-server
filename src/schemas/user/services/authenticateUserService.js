9ddf78d631a6a9117b79d7c11a6c693fb976538a
FeelipePM <feelipe@email.com>
2022-02-11 16:21:05 -0300

    create session to authenticate user

---
+++ src/schemas/user/services/authenticateUserService.js
@@ -0,0 +1,38 @@
+import { prisma } from '../../../database/prismaClient.js';
+import pkg from 'jsonwebtoken';
+import { compareHash } from '../providers/hashProvider.js';
+import authConfig from '../../../config/auth.js';
+
+const { sign } = pkg;
+
+export async function authenticateUserService(email, password) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    }
+  });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  const isPasswordValid = await compareHash(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new Error('Incorrect email / password combination.', 401);
+  }
+
+  const { secret, expiresIn } = authConfig.jwt;
+
+  const token = sign({}, secret ,{
+    subject: user.id,
+    expiresIn
+  });
+
+  const userAuthenticate = {
+    user,
+    token
+  }
+
+  return userAuthenticate;
+}

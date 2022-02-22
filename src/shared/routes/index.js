eaefb3bd9a7a2ea1321bde0fcd9a7af702d1b507
FeelipePM <feelipe@email.com>
2022-02-22 15:49:44 -0300

    routes to postimages save at database

--- src/shared/routes/index.js
+++ src/shared/routes/index.js
@@ -1,6 +1,21 @@
 import { Router } from "express";
-import ensureAuthenticated from "@shared/infrastructure/http/middlewares/ensureAuthenticated";
+import multer from "multer";
+import { storage } from "../providers/diskStorageProvider.js";
+import { postImagesController } from "../../controllers/postImagesController.js";
+import { ensureAuthenticated } from "../../../src/schemas/user/middlewares/ensureAuthenticated.js";
 
 const routes = Router();
 
-routes.use("/postImages", ensureAuthenticated);
+const upload = multer({ storage });
+
+routes.get("/", (req, res) => {
+  res.send("Server Express is Running!");
+});
+
+routes.post(
+  "/postImages",
+  [ensureAuthenticated, upload.single("file")],
+  postImagesController
+);
+
+export default routes;

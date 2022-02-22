ec3523e766e4f7c9fcfee74c664f40d8a94e7763
FeelipePM <feelipe@email.com>
2022-02-22 15:51:23 -0300

    add express to create route postImages

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ import { ApolloServer } from "apollo-server";
 import { resolvers } from "./schemas/user/resolvers.js";
 import { importSchema } from "graphql-import";
 import { ensureAuthenticated } from "././schemas/user/middlewares/ensureAuthenticated.js";
+import express from "express";
+import routes from "../src/shared/routes/index.js";
 
 const typeDefs = importSchema("./src/schemas/user/schema.graphql");
+
+const app = express();
 
 const context = ({ req }) => {
   const { user } = ensureAuthenticated(req);
@@ -19,6 +23,12 @@ const server = new ApolloServer({
   typeDefs: typeDefs,
   resolvers,
   context,
+});
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
 server.listen().then(({ url }) => {
